1eeab1dbc5b774ed3f7696aed5ded757e66854bb
Jelmer van der Meer <jelmervandermeer02@gmail.com>
2026-01-22 22:28:52 +0100

    searchbar.blade.php

---
+++ resources/views/components/searchbar.blade.php
@@ -0,0 +1,31 @@
+<div class="bg-white rounded-xl shadow-lg p-4 grid grid-cols-5 gap-2">
+
+    <input
+        type="text"
+        placeholder="Accommodation"
+        class="border rounded px-3 py-2 text-sm"
+    >
+
+    <input
+        type="text"
+        placeholder="Check-in"
+        class="border rounded px-3 py-2 text-sm"
+    >
+
+    <input
+        type="text"
+        placeholder="Check-out"
+        class="border rounded px-3 py-2 text-sm"
+    >
+
+    <input
+        type="text"
+        placeholder="Guests"
+        class="border rounded px-3 py-2 text-sm"
+    >
+
+    <x-button>
+        Search
+    </x-button>
+
+</div>
