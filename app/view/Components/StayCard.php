b5bc18f7e28cbb85f0d9c14b65967f7cbd55e84b
Jelmer van der Meer <jelmervandermeer02@gmail.com>
2026-01-22 22:17:07 +0100

    alle php klasses aangemaakt

---
+++ app/view/Components/StayCard.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+
+class StayCard extends Component
+{
+    public function render()
+    {
+        return view('components.stay-card');
+    }
+}
